3ef0d0df4dabd53259df7b0e2e5fee94f78f5fa3
EZavdovev <45925246+EZavdovev@users.noreply.github.com>
2020-06-25 03:15:30 +0200

    Task4
    Task4 is Complete

---
+++ Task4Student4/Task4Student4/Task4Student4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+double Perimeter(double a)
+{
+    return 4 * a;
+}
+
+double Square(double a)
+{
+    return pow(a, 2);
+}
+
+double Diagonal(double a)
+{
+    return sqrt(2 * pow(a, 2));
+}
+
+int main()
+{
+    double a;
+    std::cin >> a;
+    std::cout << "Perimeter: " << Perimeter(a) << "\n";
+    std::cout << "Square: " << Square(a) << "\n";
+    std::cout << "Diagonal: " << Diagonal(a) << "\n";
+    return 0;
+}
+
